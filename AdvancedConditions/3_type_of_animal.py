0112750d93a9aab0f9831b3f1ab7f7aa81095dd2
SimeonHristov93 <Simeon.hristov@yahoo.com>
2025-01-17 17:02:21 +0000

    Delete AdvancedConditions directory

--- AdvancedConditions/3_type_of_animal.py
+++
@@ -1,8 +0,0 @@
-animals_type = input()
-
-if animals_type == 'dog':
-    print("mammal")
-elif animals_type == 'crocodile' or animals_type == 'snake' or animals_type == 'tortoise':
-    print('reptile')
-else:
-    print('unknown')
